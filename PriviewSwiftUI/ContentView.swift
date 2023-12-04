24286d6a2d9d7481cc3d7b89dd1015bddd4b802a
Emir <emirseyhana.z@gmail.com>
2023-12-04 15:37:00 +0300

    Initial Commit

---
+++ PriviewSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PriviewSwiftUI
+//
+//  Created by Emir Seyhan on 4.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PriviewSwiftUI/PriviewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriviewSwiftUIApp.swift
+//  PriviewSwiftUI
+//
+//  Created by Emir Seyhan on 4.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct PriviewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
